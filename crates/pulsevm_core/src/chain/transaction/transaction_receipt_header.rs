19676baaa90eca514eba66fb008c664492cd7524
Glenn <705252+MlennGarien@users.noreply.github.com>
2026-03-18 21:18:33 +0100

    remove duplicate transaction_receipt

--- crates/pulsevm_core/src/chain/transaction/transaction_receipt_header.rs
+++ crates/pulsevm_core/src/chain/transaction/transaction_receipt_header.rs
@@ -1,11 +1,8 @@
 use std::fmt;
 
-use pulsevm_crypto::Digest;
 use pulsevm_proc_macros::{NumBytes, Read, Write};
 use pulsevm_serialization::{NumBytes, Read, ReadError, VarUint32, Write, WriteError};
 use serde::Serialize;
-
-use crate::chain::id::Id;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TransactionStatus {
@@ -56,7 +53,6 @@ impl Write for TransactionStatus {
             TransactionStatus::Executed => 0_u8.write(bytes, pos),
             TransactionStatus::SoftFail => 1_u8.write(bytes, pos),
             TransactionStatus::HardFail => 2_u8.write(bytes, pos),
-            _ => Err(WriteError::TryFromIntError),
         }
     }
 }
@@ -89,34 +85,5 @@ impl TransactionReceiptHeader {
             cpu_usage_us,
             net_usage_words,
         }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Read, Write, NumBytes)]
-pub struct TransactionReceipt {
-    pub header: TransactionReceiptHeader,
-    pub trx_id: Id,
-}
-
-impl TransactionReceipt {
-    pub fn new(
-        status: TransactionStatus,
-        cpu_usage_us: u32,
-        net_usage_words: VarUint32,
-        trx_id: Id,
-    ) -> Self {
-        TransactionReceipt {
-            header: TransactionReceiptHeader {
-                status,
-                cpu_usage_us,
-                net_usage_words,
-            },
-            trx_id,
-        }
-    }
-
-    pub fn digest(&self) -> Result<Digest, WriteError> {
-        let serialized = self.pack()?;
-        Ok(Digest::hash(serialized))
     }
 }
