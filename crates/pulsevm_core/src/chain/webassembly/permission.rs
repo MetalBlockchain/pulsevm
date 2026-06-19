3cc0d807b81ee05f8c703077fe3fb40cc2a29699
Glenn <705252+MlennGarien@users.noreply.github.com>
2026-06-19 11:45:51 +0200

    add get_account_creation_time host function

--- crates/pulsevm_core/src/chain/webassembly/permission.rs
+++ crates/pulsevm_core/src/chain/webassembly/permission.rs
@@ -14,3 +14,13 @@ pub fn get_permission_last_used(
     let last_used = db.get_permission_last_used(permission)?;
     Ok(last_used.time_since_epoch().count())
 }
+
+pub fn get_account_creation_time(
+    env: FunctionEnvMut<WasmContext>,
+    account: u64,
+) -> Result<i64, RuntimeError> {
+    let env_data = env.data();
+    let db = env_data.db();
+    let account = db.get_account(account)?;
+    Ok(account.get_creation_date().to_time_point().time_since_epoch().count())
+}
