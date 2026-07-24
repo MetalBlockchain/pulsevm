c275a5984752f184e17c50e865a63ae11ff596d1
Glenn <705252+MlennGarien@users.noreply.github.com>
2026-07-24 18:01:12 +0200

    remove unused code

--- crates/pulsevm_core/src/chain/resource/mod.rs
+++ crates/pulsevm_core/src/chain/resource/mod.rs
@@ -1,11 +1,5 @@
 mod account_resource_limit;
 pub use account_resource_limit::AccountResourceLimit;
 
-mod resource_limits_state;
-pub use resource_limits_state::ResourceLimitsState;
-
 mod resource_limits;
 pub use resource_limits::ResourceLimits;
-
-mod resource_usage;
-pub use resource_usage::ResourceUsage;
